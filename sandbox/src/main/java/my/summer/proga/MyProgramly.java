27b3c0f0b4c7a16c2c338ebbf5c63e5ed48e624a
Brighel1a <40040010+Brighel1a@users.noreply.github.com>
2018-06-20 23:54:04 +0300

    changed settings formatir

--- sandbox/src/main/java/my/summer/proga/MyProgramly.java
+++ sandbox/src/main/java/my/summer/proga/MyProgramly.java
@@ -1,7 +1,9 @@
 package my.summer.proga;
 
 public class MyProgramly {
-public static void main(String[] args){
-System.out.println ("Hello, World");
-}
+  public static void main(String[] args){
+  System.out.println ("Hello, World");
+ }
+
+
 }
