98b6cbfef8529409e0d1e2e3d230d13272622b71
Brighel1a <40040010+Brighel1a@users.noreply.github.com>
2018-07-17 00:40:09 +0300

    создание автотеста и отделение вспомогательных методов и классов

---
+++ addressbook-web-tests/src/test/java/my/summer/addressbook/GroupData.java
@@ -0,0 +1,25 @@
+package my.summer.addressbook;
+
+public class GroupData {
+  private final String groupname;
+  private final String header;
+  private final String footer;
+
+  public GroupData(String name, String header, String footer) {
+    this.groupname = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+  public String getGroupname() {
+    return groupname;
+  }
+
+  public String getHeader() {
+    return header;
+  }
+
+  public String getFooter() {
+    return footer;
+  }
+}
